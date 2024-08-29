032b18c8e2d4b30c1d213d9a68761eaa0228771f
przlaicu <41859496+przlaicu@users.noreply.github.com>
2024-08-29 18:38:55 +0200

    29.08.2024 predavanje
    predavanje od 29.08.2024

---
+++ JavaScript/Code/36 - Exam Dry Run/Task 2/starter.js
@@ -0,0 +1,17 @@
+// Create some books
+let book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "Fiction");
+let book2 = new Book("A Brief History of Time", "Stephen Hawking", "Science");
+
+/**
+ * CONSOLE OUTPUT!
+ */
+// JavaScript Basics
+// {"title":"JavaScript Basics","author":"John Doe","year":2023}
+// Added 2 books to the library.
+// The Great Gatsby has been borrowed.
+// Discount for Science books: 20%
+
+//         Library Name: City Central Library
+//         Total Books: 1000
+//         Available Genres: Fiction, Non-Fiction, Science, History, Biography, Mystery, Romance
+    
