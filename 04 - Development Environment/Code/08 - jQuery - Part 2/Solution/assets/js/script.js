9c6e224cb0c93d65fe90dab663f45c330a3cb0c5
przlaicu <41859496+przlaicu@users.noreply.github.com>
2024-09-06 21:05:54 +0200

    06.09.2024 jquery vježbe
    rješenja od profa

--- 04 - Development Environment/Code/08 - jQuery - Part 2/Solution/assets/js/script.js
+++ 04 - Development Environment/Code/08 - jQuery - Part 2/Solution/assets/js/script.js
@@ -7,7 +7,6 @@ $(document).ready(function () {
       const compiled_template = Handlebars.compile(template);
       const rendered = compiled_template({ user: data });
       document.getElementById('users').innerHTML = rendered;
-      addStripes();
       afterRender();
     },
   });
@@ -17,7 +16,21 @@ $(document).ready(function () {
   }
 
   function afterRender() {
-    $('[data-toggle="popover"]').popover(); // Initialize popovers
+    $('[data-toggle="popover"]').popover();
     $('table th').css('color', 'darkBlue');
+
+    $('table tr td').on('mouseenter', (event) => {
+      $(event.currentTarget).css('backgroundColor', 'yellow');
+    });
+
+    $('table tr td').on('mouseleave', (event) => {
+      $(event.currentTarget).removeAttr('style');
+    });
+
+    addStripes();
   }
+
+  $(window).resize(() => {
+    console.log($(window).width());
+  });
 });
---
+++ 04 - Development Environment/Code/08 - jQuery - Part 2/Task/Instructions.md
@@ -0,0 +1,6 @@
+## Zadatak 📝
+
+1. Pomoću jQueryja:
+	- Pratite resize događaj na korisničkom ekranu ispisujući širinu ekrana u konzolu
+	- Pratite kretanje miša i na ulazak miša preko retka tablice promijenite mu pozadinsku boju
+	- Na izlazak miša sa retka mora se pozadinska boja vratiti na prijašnju
